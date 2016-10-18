4bd89c262de673da8e4bdc8a075973031f9057ca
Aprian1004 <indraaprian1004@gmail.com>
2016-10-18 23:37:46 +0700

    Update autokill.sh

--- autokill.sh
+++ autokill.sh
@@ -1,13 +1,13 @@
-#while :
- # do 
-  #./userlimit.sh 1 
-  #sleep 5 
-  #done
-#while :
-  #do
-  #./userlimit.sh 2
-  #sleep 55 
-  #done
+while :
+ do 
+  ./userlimit.sh 1 
+  sleep 5 
+  done
+while :
+  do
+  ./userlimit.sh 2
+  sleep 55 
+  done
 while :
   do
   user-expire
